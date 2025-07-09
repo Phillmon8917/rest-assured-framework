3c5e431140ef7bb1f8f4c729881394f149e4a9e8
Motsinoni Phillimon <Motsinoni.p955@gmail.com>
2025-07-09 04:19:20 +0200

    Updated the test priority on the getbookings as it collided

--- src/test/java/bookingapitest/GetBookingApiTests.java
+++ src/test/java/bookingapitest/GetBookingApiTests.java
@@ -14,13 +14,13 @@ public class GetBookingApiTests {
         this.getBookingApi = new GetBookingApi();
     }
 
-    @Test(description = "Basic HTTP Status check for get booking ids API")
+    @Test(priority = 1)
     public void validateStatusCodeForGetBookingIdApi(){
           this.getBookingApi.getAllBookingIds()
                 .then().assertThat().statusCode(200);
     }
 
-    @Test(description =  "Basic HTTP Status check for get booking by ID API")
+    @Test(description =  "Basic HTTP Status check for get booking by ID API", priority = 2)
     public void validateStatusCodeForGetBookingByIdAPI(){
         this.getBookingApi.getBookingById(20)
                 .then().assertThat().statusCode(200);
